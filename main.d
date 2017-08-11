f7b53521d5b00e377347be6aac4f32b5eb966527
bgroves <bgroves@corp.adtran.com>
2017-08-11 08:47:44 -0500

    submitted version with geometric functions

--- main.d
+++ main.d
@@ -1,27 +1,26 @@
-import std.array;
-import std.algorithm;
-import std.stdio;
-import std.exception;
 import std.string;
 import std.conv;
 import std.regex;
 import std.math;
+import core.stdc.stdlib;
 
-static double doTheMath(double firstNum, double secondNum, string sign)
+extern(C) int strcmp(const char* s1, const char* s2);
+
+double doTheMath(double firstNum, double secondNum, string sign)
 {
-	if(sign == "-")
+	if(strcmp(sign.toStringz,"-") == 0)
 	{
 		return firstNum - secondNum;
 	}
-	else if(sign == "+")
+	else if(strcmp(sign.toStringz,"+") == 0)
 	{
 		return firstNum + secondNum;
 	}
-	else if(sign == "*")
+	else if(strcmp(sign.toStringz,"*") == 0)
 	{
 		return firstNum * secondNum;
 	}
-	else if(sign == "/")
+	else
 	{
 		if(secondNum == 0.0)
 		{
@@ -32,7 +31,6 @@ static double doTheMath(double firstNum, double secondNum, string sign)
 			return firstNum / secondNum;
 		}
 	}
-	return 0;
 }
 
 unittest
@@ -52,6 +50,18 @@ unittest
 	}
 }
 
+double doGeometry(double number, string operator)
+{
+	if(strcmp(operator.toLower.toStringz,"sin") == 0)
+	{
+		return number.sin;
+	}
+	else
+	{
+		return number.cos;
+	}
+}
+
 double getArithmeticOperatorCount(string stringForOper)
 {
 	auto capture = matchAll(stringForOper, r"\+|\-|\*|\/");
@@ -65,32 +75,49 @@ double getArithmeticOperatorCount(string stringForOper)
 unittest
 {
 	string input = "1.0 2.0 +";
-	double operatorCount = input.getArithmeticOperatorCount();
+	double operatorCount = input.getArithmeticOperatorCount;
 	assert(operatorCount == 1);
 	input = "1.0 2.0 3.0 + -";
-	operatorCount = input.getArithmeticOperatorCount();
+	operatorCount = input.getArithmeticOperatorCount;
 	assert(operatorCount == 2);
 }
 
-static double doTheStuff(string inputString)
+double getGeometricOperatorCount(string stringForOper)
 {
+	auto capture = matchAll(stringForOper.toLower, r"sin|cos");
+	if(!capture.empty)
+	{
+		return capture.count;
+	}
+	return 0.0;
+}
+
+unittest
+{
+	string input = "1.0 sin";
+	double operatorCount = input.getGeometricOperatorCount;
+	assert(operatorCount == 1);
+	input = "1.0 sin cos";
+	operatorCount = input.getGeometricOperatorCount;
+	assert(operatorCount == 2);
+}
+
+double calculateValueFromString(string inputString)
+{
+	import std.array;
+	import std.algorithm;
+
 	string[] inputArray = inputString.split(" ");
 	if(inputArray.length == 0)
 	{
 		throw new StringException("Empty string");
 	}
 
-	/*double operatorCount = inputString.getArithmeticOperatorCount();
-	if((operatorCount < ((inputArray.length/2))))
-	{
-		throw new StringException("No ending operator");
-	}*/
-
 	double[] outputArray;
 
 	while(inputArray.length > 0)
 	{
-		string element = inputArray.front();
+		string element = inputArray.front;
 		inputArray = inputArray.remove(0);
 		try
 		{
@@ -107,9 +134,9 @@ static double doTheStuff(string inputString)
 				}
 				else
 				{
-					double secondNum = outputArray.back();
+					double secondNum = outputArray.back;
 					outputArray = outputArray.remove(outputArray.length-1);
-					double firstNum = outputArray.back();
+					double firstNum = outputArray.back;
 					outputArray = outputArray.remove(outputArray.length-1);
 					double currentVal = doTheMath(firstNum, secondNum, element);
 					inputArray.insertInPlace(0,to!string(currentVal));
@@ -119,8 +146,16 @@ static double doTheStuff(string inputString)
 			{
 				if(outputArray.length < 1)
 				{
-					throw new StringException("To few values for " + element);
+					throw new StringException(("To few values for " ~ element));
 				}
+				double onlyNum = outputArray.back;
+				outputArray = outputArray.remove(outputArray.length-1);
+				double currentVal = doGeometry(onlyNum, element);
+				inputArray.insertInPlace(0,to!string(currentVal));
+			}
+			else
+			{
+				throw new StringException(("Invalid character " ~ element));
 			}
 		}
 	}
@@ -130,34 +165,34 @@ static double doTheStuff(string inputString)
 unittest
 {
 	string input = "1.0 2.0 +";
-	double result = input.doTheStuff();
+	double result = input.calculateValueFromString;
 	assert(result == 3.0);
 	input = "1.0 2.0 -";
-	result = input.doTheStuff();
+	result = input.calculateValueFromString;
 	assert(result == -1.0);
 	input = "1.0 2.0 *";
-	result = input.doTheStuff();
+	result = input.calculateValueFromString;
 	assert(result == 2.0);
 	input = "1.0 2.0 /";
-	result = input.doTheStuff();
+	result = input.calculateValueFromString;
 	assert(result == 0.5);
 	input = "1.0 2.0 + 3.0 -";
-	result = input.doTheStuff();
+	result = input.calculateValueFromString;
 	assert(result == 0.0);
 	input = "1.0 2.0 - 3.0 +";
-	result = input.doTheStuff();
+	result = input.calculateValueFromString;
 	assert(result == 2.0);
 	input = "1.0 2.0 * 2.0 /";
-	result = input.doTheStuff();
+	result = input.calculateValueFromString;
 	assert(result == 1.0);
 	input = "1.0 2.0 / 3.0 *";
-	result = input.doTheStuff();
+	result = input.calculateValueFromString;
 	assert(result == 1.5);
 
 	try
 	{
 		input = "1.0 2.0 0.0 / -";
-		result = input.doTheStuff();
+		result = input.calculateValueFromString;
 		assert(false);
 	}
 	catch (StringException e)
@@ -168,7 +203,7 @@ unittest
 	try
 	{
 		input = "";
-		result = input.doTheStuff();
+		result = input.calculateValueFromString;
 		assert(false);
 	}
 	catch (StringException e)
@@ -177,13 +212,13 @@ unittest
 	}
 
 	input = "1.0";
-	result = input.doTheStuff();
+	result = input.calculateValueFromString;
 	assert(result == 1.0);
 
 	try
 	{
 		input = "1.0 + 1.0";
-		result = input.doTheStuff();
+		result = input.calculateValueFromString;
 		assert(false);
 	}
 	catch (StringException e)
@@ -192,15 +227,53 @@ unittest
 	}
 
 	input = "1.0 1.0 1.0 1.0 + - *";
-	result = input.doTheStuff();
+	result = input.calculateValueFromString;
 	assert(result == -1);
 
 	input = "1.0 1.0 + sin";
-	result = input.doTheStuff();
-	assert(result == sin(2.0));
+	result = input.calculateValueFromString;
+	string resultString = to!string(result);
+	string expectedString = to!string(sin(2.0));
+	assert(resultString == expectedString);
+
+	input = "1.0 1.0 + cos";
+	result = input.calculateValueFromString;
+	resultString = to!string(result);
+	expectedString = to!string(cos(2.0));
+	assert(resultString == expectedString);
+
+	input = "1.0 1.0 + SIN";
+	result = input.calculateValueFromString;
+	resultString = to!string(result);
+	expectedString = to!string(sin(2.0));
+	assert(resultString == expectedString);
+
+	input = "1.0 1.0 + COS";
+	result = input.calculateValueFromString;
+	resultString = to!string(result);
+	expectedString = to!string(cos(2.0));
+	assert(resultString == expectedString);
 
 }
 
-void main() {
-	return;
+void main()
+{
+	import std.stdio;
+	string line = "";
+	writeln("enter exit to exit");
+	while ((line = readln.strip) !is null)
+	{
+		if(line.toLower == "exit")
+		{
+			exit(0);
+		}
+		try
+		{
+			writeln(line.calculateValueFromString);
+		}
+		catch (StringException e)
+		{
+			writeln(e.msg);
+		}
+	}
 }
